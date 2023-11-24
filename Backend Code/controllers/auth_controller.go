e446fec2ec1fa0cf2aa61399c2c194908b76ba97
randomplayer2002 <amith.hathwar02@gmail.com>
2023-11-24 20:33:26 +0530

    Done

---
+++ Backend Code/controllers/auth_controller.go
@@ -0,0 +1,21 @@
+// controllers/auth_controller.go
+
+package controllers
+
+import (
+	"net/http"
+	"your-package/models"
+	"your-package/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Register handles user registration
+func Register(c *gin.Context) {
+	// Implement user registration logic
+}
+
+// Login handles user login
+func Login(c *gin.Context) {
+	// Implement user login logic
+}
